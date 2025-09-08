b27cce37efcbe8d40b88beace42f3ce4e10a0474
JosephTocre <u22304704@utp.edu.pe>
2025-09-08 02:41:11 -0500

    Primer avance

---
+++ src/main/resources/dbcine/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO pelicula (titulo, genero, duracion, imagen) VALUES
+('Demon Slayer: Castillo Infinito', 'Anime', 150, 'demon_slayer_castillo_infinito.jpg'),
+('Cazadores del fin del mundo', 'Aventura', 105, 'cazadores_del_fin_del_mundo.jpg'),
+('Toy Story (30 Aniversario)', 'Aniversario', 100, 'toy_story_30_aniversario.jpg'),
+('Camina o muere', 'Drama', 130, 'camina_o_muere.jpg'),
+('El gran viaje de tu vida', 'Familiar', 105, 'el_gran_viaje_de_tu_vida.jpg'),
+('Otro viernes de locos', 'Comedia', 110, 'otro_viernes_de_locos.jpg'),
+('La hora de la desaparición', 'Suspenso', 125, 'la_hora_de_la_desaparicion.jpg'),
+('El conjuro 4', 'Terror', 115, 'el_conjuro_4.jpg'),
+('Cómo entrenar a tu dragón', 'Animación', 105, 'como_entrenar_a_tu_dragon.jpg'),
+('Los 4 fantásticos: Primeros pasos', 'Superhéroes', 120, 'los_4_fantasticos_primeros_pasos.jpg');
